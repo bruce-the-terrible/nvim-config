d19a5253b16ede3fb8eb489488fbfc5823586afd
brucegong <brucegong@gmail.com>
2023-08-12 23:01:42 +0800

    Add files via upload

---
+++ init.lua
@@ -0,0 +1,20 @@
+vim.g.python3_host_prog = '/opt/homebrew/bin/python3'
+
+require("core.options")
+require("core.keymaps")
+require("plugins.plugins-setup")
+
+require("plugins.lualine")
+require("plugins.nvim-tree")
+require("plugins.treesitter")
+require("plugins.lsp")
+require("plugins.cmp")
+require("plugins.autopairs")
+require("plugins.bufferline")
+require("plugins.gitsigns")
+require("plugins.telescope")
+require("plugins.comment")
+require("plugins.symbol_outline")
+
+
+
